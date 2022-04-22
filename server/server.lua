1be9ab6c7d80a9b6fb8604fc172f887d589213a9
TopicElite <75167734+TopicElite@users.noreply.github.com>
2022-04-22 16:23:13 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,30 +1,40 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
+local ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -- Events
 
 RegisterServerEvent("decrypto:server:removeitem")
 AddEventHandler("decrypto:server:removeitem", function(data)
 	local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
+	local xPlayer = ESX.GetPlayerFromId(src)
 	local item = data
-	Player.Functions.RemoveItem(item, 1)
-	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], 'remove', 1)
-	if not Player.PlayerData.metadata['stress'] then
-		Player.PlayerData.metadata['stress'] = 0
-	end
-	local newStress = Player.PlayerData.metadata['stress'] + Config.AddStress
-	if newStress <= 0 then newStress = 0 end
-	if newStress > 100 then newStress = 100 end
-	Player.Functions.SetMetaData('stress', newStress)
-    TriggerClientEvent('hud:client:UpdateStress', src, newStress)
-	TriggerClientEvent('QBCore:Notify', src, "You are feeling more stressed!", 'error', 1500)
+	xPlayer.removeInventoryItem(item, 1)
+	xPlayer.triggerEvent('esx_status:add','stress',( Config.AddStress * 1000))
+	xPlayer.showNotification("You are feeling more stressed!")
 end)
 
 RegisterNetEvent('decrypto:server:givecash')
 AddEventHandler('decrypto:server:givecash', function()
 	local src = source
-	local pData = QBCore.Functions.GetPlayer(src)
-	TriggerClientEvent("QBCore:Notify", src, "ATM cash received: $" .. Config.Withdrawal, "success", 7000)
-	pData.Functions.AddMoney('cash', Config.Withdrawal, "ATM")
+	local xPlayer = ESX.GetPlayerFromId(src)
+    local randomMoney = math.random(Config.MinWithdrawl ,Config.MaxWithdrawal)
+	xPlayer.showNotification("ATM cash received: $" .. randomMoney)
+	xPlayer.addAccountMoney(Config.Account, randomMoney)
+end)
+
+ESX.RegisterServerCallback('decrypto:server:hasitem', function(source, cb, item1, item2)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local hasItems = false
+    local hasItem1 = false
+    local hasItem2 = false
+    if xPlayer.getInventoryItem(item1).count > 0 then
+        hasItem1 = true
+    end
+    if xPlayer.getInventoryItem(item2).count > 0 then
+        hasItem2 = true
+    end
+    if hasItem1 and hasItem2 then
+        hasItems = true
+    end
+    cb(hasItems)
 end)
