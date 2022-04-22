8fc702197aef43fa1832bceb32d3618639023113
TopicElite <75167734+TopicElite@users.noreply.github.com>
2022-04-22 16:23:50 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,22 @@
 Config = {}
 
-Config.Withdrawal = 500 -- $ received for successful hack
+Config.MinWithdrawl = 400 -- $ received minimum for successful hack
+Config.MaxWithdrawal = 500 -- $ received maximum for successful hack
+Config.Account = "black_money" -- money | black_money | bank
 Config.MingameBase = 10 -- 10 for decimal or 16 for hexadecimal
-Config.MinigameTimeLimit = 50 -- time limit in seconds
+Config.MinigameTimeLimit = 100 -- time limit in seconds
 Config.MingameAttemptLimit = 10 -- number of guesses allowed
-Config.AddStress = 5 -- % stress gain from hacking
+Config.AddStress = 5 --% stress gain from hacking
+
 
 Config.ATMModels = {
+    `prop_atm_01`, -- 20 on map
+    `prop_atm_02`, -- 37 on map
+    `prop_atm_03`, -- 30 on map
+    `prop_fleeca_atm` -- 4 on map
+}
+
+Config.ATMModelsString = {
     "prop_atm_01", -- 20 on map
     "prop_atm_02", -- 37 on map
     "prop_atm_03", -- 30 on map
